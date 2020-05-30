05403bbfb6860d355c935491375711f1c4f5b805
yuval <yshechter@umass.edu>
2020-05-29 22:24:47 -0400

    updated about me button

--- components.js
+++ components.js
@@ -1,8 +1,10 @@
 window.onload = function() {
     this.document.getElementById("about_me_modal_open").addEventListener("click", function(){
-        document.getElementById('about_me').style.display='block';
+        document.getElementById('about_me_modal_close').style.display='block';
+        document.getElementById('about_me_modal_open').style.display='none';
     });
     this.document.getElementById("about_me_modal_close").addEventListener("click", function(){
-        document.getElementById('about_me').style.display='none';
+        document.getElementById('about_me_modal_close').style.display='none';
+        document.getElementById('about_me_modal_open').style.display='block';
     });
 }
